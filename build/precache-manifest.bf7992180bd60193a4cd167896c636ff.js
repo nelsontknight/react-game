1b83b9c6bb0f15db3becf5d31f75e872093f835c
Nelson Knight <nelson.knight92@gmail.com>
2020-01-15 16:55:23 -0700

    touch up

---
+++ build/precache-manifest.bf7992180bd60193a4cd167896c636ff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8aa7690e3dbd8730564ce0765849e164",
+    "url": "/react-game/index.html"
+  },
+  {
+    "revision": "0bb0a19e897dfa101187",
+    "url": "/react-game/static/css/main.06205fa9.chunk.css"
+  },
+  {
+    "revision": "a4d43337e60fc5f4725c",
+    "url": "/react-game/static/js/2.26f80e77.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-game/static/js/2.26f80e77.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0bb0a19e897dfa101187",
+    "url": "/react-game/static/js/main.711295b5.chunk.js"
+  },
+  {
+    "revision": "b0faca6c9fdd18ce8091",
+    "url": "/react-game/static/js/runtime-main.cfa399d1.js"
+  }
+]);
